c1a89a084dffa135d3f0e75bb829cf6bd358bb30
HannaNguyen-new <hanchan1305@gmail.com>
2021-03-16 22:06:06 +0900

    edit create-list popip

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,11 @@
 const createBtn = document.querySelector(".create-btn");
 const overlay = document.querySelector(".overlay");
+const listNameInput = document.querySelector(".list-name-input-container");
 
 function toggleOverlay(){
-   overlay.toggleAttribute("hidden")
+   overlay.toggleAttribute("hidden");
+   listNameInput.toggleAttribute("hidden");
+
 };
 
 createBtn.addEventListener("click", toggleOverlay);
