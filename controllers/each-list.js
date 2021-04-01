71ddb41e80e8252aaae4d06cadf4655a37ba2666
hanh <hanchan1305@gmail.com>
2021-04-01 23:44:09 +0900

    able to display items

--- controllers/each-list.js
+++ controllers/each-list.js
@@ -29,21 +29,20 @@ export const renderAll = function(req,res){
    .then(results => res.render("../views/pages/each-list-page",{list_name: results[0],itemArr: results[1]}))
    .catch(err => console.log(err))
 }
-export const updateListName = function(req,res){
+export const updateListName = async function(req,res){
   const id = req.params.id;
   const updatedName = req.body.listName;
-  findAndUpdateListName(id,updatedName)
+  await findAndUpdateListName(id,updatedName)
   .then(result => res.render("../views/pages/each-list-page",{list_name: result.listName}))
   .catch(err => console.log(err))
 }
 
-export const addItem = function(req,res){
+export const addItem = async function(req,res){
   const id = req.params.id;
   const item_name = req.body.itemName;
   const item_quantity = 1;
- findAndAddItem(id, item_name, item_quantity)
-  //.then(arr => res.render("../views/pages/each-list-page",{itemArr: arr}))
-  .then(result => res.redirect(result.url ))
+ await findAndAddItem(id, item_name, item_quantity)
+  .then(result => res.json(result.url))
   .catch(err => console.log(err))
 }
 
